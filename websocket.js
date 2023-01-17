7c6f25a633e311e08a8bab4a189feca7e57aea89
Lgbdel16 <116641685+Lgbdel16@users.noreply.github.com>
2023-01-17 22:04:22 +0100

    Add files via upload

---
+++ websocket.js
@@ -0,0 +1,14 @@
+<var socket = new WebSocket("ws://localhost:8080/*");
+
+socket.message = onMessage;
+
+function onMessage(event){
+      var message = event.data;
+      
+      $("#chatContent").append("<span class=\"message\">" + message + "</span>");
+}
+
+function sendMessage(){
+      var message = $("#message").val();
+      socket.send(message);
+      }
